aa767f8f0043b437de819c35193132403f73180c
Ahsan Masood <ahsan.masood111@gmail.com>
2024-11-08 19:45:13 +0500

    delete button added

--- src/js/script.js
+++ src/js/script.js
@@ -96,6 +96,9 @@ function updatePinnedDocsList() {
         t.appendChild(textSpan);
 
         var n = document.createElement("span");
+        (n.innerHTML = `
+            <svg fill="#fff" width="16" height="16" viewBox="-3 -2 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-trash-f"><path d="M12 2h5a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h5V1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1zm3.8 6-.613 9.2a3 3 0 0 1-2.993 2.8H5.826a3 3 0 0 1-2.993-2.796L2.205 8zM7 9a1 1 0 0 0-1 1v7a1 1 0 0 0 2 0v-7a1 1 0 0 0-1-1m4 0a1 1 0 0 0-1 1v7a1 1 0 0 0 2 0v-7a1 1 0 0 0-1-1"></path></svg>
+         `),
         n.style.cursor = "pointer";
         n.style.marginLeft = "10px";
         n.addEventListener("click", () => {
